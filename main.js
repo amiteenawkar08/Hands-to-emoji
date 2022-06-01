c1a33253807612127f89e15b79e1aa48de33f8ad
amiteenawkar08 <93813741+amiteenawkar08@users.noreply.github.com>
2022-06-01 15:52:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ Webcam.set({
     }
     console.log("ml5 version:" , ml5.version);
     
-    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/iTCgJucp4/model.json' , modelLoaded);
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZTz6MlGMY/model.json' , modelLoaded);
     
     function modelLoaded(){
         console.log("model Loaded");
@@ -31,4 +31,39 @@ Webcam.set({
         var utterThis=new SpeechSynthesisUtterance(speak_date_1+speak_date_2);
         synth.speak(utterThis);
     
+    }
+    function check(){
+        img = document.getElementById('captured_image');
+        classifier.classify(img,gotResult);
+    }
+    function gotResult(error, results){
+        if(error){
+            console.error(error);
+        }else{
+            console.log(results);
+            document.getElementById("result_emotion_name").innerHTML = results[0].label;
+            document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+            prediction_1 = results[0].label;
+            prediction_2 = results[1].label;
+            speak();
+            if(results[0].label == "Thumbs up"){
+                document.getElementById("update_emoji").innerHTML = "&#128077;";
+            }
+            if(results[0].label == "Thumbs down"){
+                document.getElementById("update_emoji").innerHTML = "&#128078;";
+            }
+            if(results[0].label == "Victory"){
+                document.getElementById("update_emoji").innerHTML = "&#9996;;";
+            }
+    
+            if(results[1].label == "Thumbs up"){
+                document.getElementById("update_emoji2").innerHTML = "&#128077;";
+            }
+            if(results[1].label == "Thumbs down"){
+                document.getElementById("update_emoji2").innerHTML = "&#128078;";
+            }
+            if(results[1].label == "Victory"){
+                document.getElementById("update_emoji2").innerHTML = "&#9996;;";
+            }
+        }
     }
